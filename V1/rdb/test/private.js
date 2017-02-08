6fc5302eabdad309380ea5737dcd2e741a8a3f26
David <dkeeney@travelbyroad.net>
2017-02-08 13:24:43 -0700

    Initial commit

---
+++ V1/rdb/test/private.js
@@ -0,0 +1,17 @@
+
+
+/**
+ * Created by JetBrains WebStorm.
+ * User: David
+ * Date: 4/15/12
+ * Time: 6:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var domain = 'www.rdbhost.com',
+    acct_number = 14,
+    demo_r_role = 'r0000000014',
+    demo_p_role = 'p0000000014',
+    demo_s_role = 's0000000014',
+    demo_email = 'jsdemos@travelbyroad.net';
